d2a69170bf9551cf5fb51de9c285620fefcf5e3c
FFrancoLopez <ffedericolopezz@gmail.com>
2025-03-28 23:01:02 -0300

    configuracion de swagger y del JWT y creacion de middleware

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ArticlesModule } from './articles/articles.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ArticlesModule,
   ],
   controllers: [],
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,15 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
 
-  // const swaggerConfig = new DocumentBuilder()
-  //   .setTitle('Prueba Técnica de Flexxus')
-  //   .setDescription('API ')
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Prueba Técnica de Flexxus')
+    .setDescription('API para el proyecto de prueba técnica de Flexxus')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-  await app.listen(process.env.PORT ?? 3000);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document, {customSiteTitle: 'Prueba Técnica de Flexxus'});
+
+  const loggerMiddleware = new LoggerMiddleware();
+  app.use(loggerMiddleware.use)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  )
+
+  const port = process.env.PORT || 3001; 
+
+  await app.listen(port, () => {
+    console.log(`🚀 Servidor corriendo en el puerto ${port}`);
+  });
 }
 bootstrap();
